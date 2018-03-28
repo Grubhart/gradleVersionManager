eee605ba433342321416c20ca8662f815bf34213
grubhart <grubhart@gmail.com>
2018-03-28 12:21:04 -0500

    Initial commit

---
+++ src/main/java/org/grubhart/ToDoApp.java
@@ -0,0 +1,9 @@
+package org.grubhart;
+
+public class ToDoApp {
+    public static final char DEFAULT_INPUT = '\u0000';
+
+    public static void main(String args[]) {
+        System.out.println("hola mundo");
+    }
+}
